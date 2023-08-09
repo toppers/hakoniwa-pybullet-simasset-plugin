8620d1fbaeb86fe44ebe2ffd30852d96c5916a52
Takashi Mori <kanetugu2015@gmail.com>
2023-08-09 19:32:17 +0900

    add sample

--- src/phys_impl/hako_phys_pybullet.py
+++ src/phys_impl/hako_phys_pybullet.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pybullet as p
+import pybullet_data
+import numpy as np
+import time
 
 from hako_phys_ops import HakoPhysOps
 
 class HakoPhysPybullet(HakoPhysOps):
     def __init__(self):
-        pass
+        self.physicsClient = p.connect(p.GUI)
+        p.resetSimulation()
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-10)
+        timestep = 1. / 60.
+        p.setTimeStep(timestep)
+        self.planeId = p.loadURDF("../bullet3/data/plane.urdf")
+        StartPos = [0,0,0]  
+        StartOrient = p.getQuaternionFromEuler([0,0,3.14]) 
+        self.carId = p.loadURDF("../bullet3/data/racecar/racecar.urdf",StartPos, StartOrient)
 
     def initialize(self):
         pass
@@ -14,7 +27,26 @@ class HakoPhysPybullet(HakoPhysOps):
         pass
 
     def step(self):
-        pass
+        p.setJointMotorControl2(self.carId, 2, p.VELOCITY_CONTROL, targetVelocity=10)
+        p.setJointMotorControl2(self.carId, 3, p.VELOCITY_CONTROL, targetVelocity=40)
+        p.setJointMotorControl2(self.carId, 5, p.VELOCITY_CONTROL, targetVelocity=10)
+        p.setJointMotorControl2(self.carId, 7, p.VELOCITY_CONTROL, targetVelocity=40)
+        p.stepSimulation()
 
     def reset(self):
         pass
+
+
+if __name__ == "__main__":
+    print("INITIALIZING")
+
+    phys = HakoPhysPybullet()
+
+    input("ENTER")
+    print("START")
+
+    while True:
+        phys.step()
+        time.sleep(1. / 60.)
+    
+    print("END")
